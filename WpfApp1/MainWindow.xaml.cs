f3b3218a2b32bd59d95a1a97f252d8e628f678c6
andreyzr43 <andreyzr43@yandex.ru>
2023-05-31 21:26:15 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ открыт");
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ сохранен");
+        }
+
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtBox1!=null && txtBox1.Text.Length == 0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+    }
+}
---
+++ WpfApp1/MyCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Exit {  get;  set; }
+        static MyCommands()
+        {
+            InputGestureCollection input=new InputGestureCollection();
+            input.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit =new RoutedUICommand("Выход","Exit",typeof(MyCommands), input);
+        }
+    }
+
+}
